186c112cd792351aa88252dc79ecb32b13e0c980
Rikuto <orikutobasuke@gmail.com>
2023-03-07 18:54:42 +0900

    nextjs add

--- src/pages/sample/02/index.tsx
+++ src/pages/sample/02/index.tsx
@@ -1,1 +1,26 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {Box,Text,Center,Input} from '@chakra-ui/react'
+
+function rolldice(){
+    return Math.floor(Math.random()*6 + 1)
+}
+
+export default function Sample02(){
+
+    const [state,setState] = useState(0)
+
+    useEffect(()=>{
+        setState(rolldice())
+    },[])    
+
+    return(
+        <>
+            <Box>
+                <Text as={'p'}>サイコロの目:{state}</Text>
+                <Input type='button' value='ふる'
+                    onClick={()=> setState(rolldice())}
+                />
+            </Box>
+        </>
+    )
+}
