182827273b2ba07cad7644549e74f3ff0f4e6aa7
Rikuto <orikutobasuke@gmail.com>
2023-03-08 17:47:42 +0900

    nextjs study

---
+++ src/pages/chakra/02/index.tsx
@@ -0,0 +1,11 @@
+import {Box,Center,Flex,Input,Text} from '@chakra-ui/react'
+
+export default function ChakraUI02(){
+    return(
+        <>
+            <Box>
+                
+            </Box>
+        </>
+    )
+}
