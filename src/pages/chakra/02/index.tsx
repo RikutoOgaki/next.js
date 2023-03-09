2de622067399ddf93875e81353e34bfff0f0e041
Rikuto <orikutobasuke@gmail.com>
2023-03-09 13:25:31 +0900

    nextjs add

--- src/pages/chakra/02/index.tsx
+++ src/pages/chakra/02/index.tsx
@@ -4,7 +4,13 @@ export default function ChakraUI02(){
     return(
         <>
             <Box>
-                
+                <Text as={'h1'}>レスポンス対応しているfontSizeについて</Text>
+                <Box>
+                    <Text 
+                        as={'h2'}
+                        fontSize={{base:'5xl',md:'3xl'}}
+                        >mainContents</Text>
+                </Box>
             </Box>
         </>
     )
