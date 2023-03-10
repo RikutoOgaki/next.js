b826a88e5cc4fb6df5a1e7097e66fa2f4c4e1c58
Rikuto <orikutobasuke@gmail.com>
2023-03-10 14:54:56 +0900

    nextjs add

--- src/pages/first/filter/index.tsx
+++ src/pages/first/filter/index.tsx
@@ -3,21 +3,23 @@ import {Box,Flex,Center,Text} from '@chakra-ui/react'
 
 export default function Filter(){
 
-    const array = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
-
+    const array = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present','aaaaaa']
+    
     // filterは元のある配列から新しい配列を生成するが、アロー関数以降の条件と一致する関数（いわばtrue）のものだけを残し、残りは除外する
-
-
     const result = array.filter(map => map.length>6)
     // 文字数が６文字以上のものを残す
 
+    const result2 = array.filter(mini => mini.length===6)
+
     console.log(result);
+    console.log(result2);
+    
     
 
     return(
         <>
             <Box>
-                <Text as={'h1'}>mapについて</Text>
+                <Text as={'h1'}>filterについて</Text>
                 <Text as={'p'}>console.logを確認する</Text>
             </Box>
         </>
--- src/pages/first/map/index.tsx
+++ src/pages/first/map/index.tsx
@@ -1,10 +1,26 @@
 import {useState} from 'react'
 import {Box,Flex,Center,Text,Input} from '@chakra-ui/react'
 
+
 export default function Map(){
+
+    const array = [1,2,3,4,5]
+
+    const array2 = array.map(x => x*5)
+    // mapは配列のある数だけ新しい配列を生成する
+    // xの引数にはarrayの値が入っていて、アロー関数以降は処理が書かれている
+    // （今回は、もともとあったarrayを新しい配列で生成して、その値に３かけた数字が配列として在る）
+
+    console.log(array2);
+    
+
+
     return(
         <>
-            
+            <Box>
+                <Text as={'h1'}>mapについて</Text>
+                <Text as={'p'}>console.logを確認する</Text>
+            </Box>
         </>
     )
 }
