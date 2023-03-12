4627240a2fc67f91f06243b825cbbd8adc6d34a2
Rikuto <orikutobasuke@gmail.com>
2023-03-12 22:28:13 +0900

    nextjs add

---
+++ src/pages/properness/02/index.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+import {Box,Flex,Text,Input} from '@chakra-ui/react'
+
+export default function Number02(){
+    return(
+        <>
+            <Box>
+                
+            </Box>
+        </>
+    )
+}
