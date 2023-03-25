fe3607bf77c0c5b609c098c3c76d1e7429afb291
Rikuto <orikutobasuke@gmail.com>
2023-03-25 17:29:02 +0900

    next.js

---
+++ src/pages/sample/20/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { Box, Flex, Input, Text } from '@chakra-ui/react'
+
+export default function Sample20() {
+    return (
+        <>
+
+        </>
+    )
+}
