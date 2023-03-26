c8537cc9c7178200736ad84bfc87dfbba2b0b6dc
Rikuto <orikutobasuke@gmail.com>
2023-03-26 23:03:37 +0900

    next.js

--- src/pages/properness/05/component.tsx
+++ src/pages/properness/05/component.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 import { Box, Flex, Text } from '@chakra-ui/react'
 
 export function Number05Component() {
+
+    const [state, setState] = useState({
+
+    })
+
     return (
-        <></>
+        <>
+            <Box></Box>
+        </>
     )
 }
